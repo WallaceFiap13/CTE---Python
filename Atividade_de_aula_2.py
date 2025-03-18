3ec2a381730cdba448069593dd5d98d58d483bbb
WallaceFiap13 <rm566217@fiap.com.br>
2025-03-17 22:40:52 -0300

    Atividade da aula 17/03

---
+++ Atividade_de_aula_2.py
@@ -0,0 +1,14 @@
+print("\n")
+print("Insira os números x, y & z e obtenha a soma")
+
+x=int(input("Número X = "))
+y=int(input("Número y = "))
+z=int(input("Número z = "))
+
+soma=x+y+z
+
+print("\n")
+print("A soma é:")
+
+print("\n")
+print(soma)

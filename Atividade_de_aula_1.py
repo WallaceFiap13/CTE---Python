3c5176863c4f0f0699666b0c61f0152c7de71225
WallaceFiap13 <rm566217@fiap.com.br>
2025-03-17 22:26:30 -0300

    Atividade da aula 17/03

---
+++ Atividade_de_aula_1.py
@@ -0,0 +1,25 @@
+# Exercício 1 -------------------
+
+print("\n")
+print("Wallace Queiroz de Lima")
+
+# Exercício 2 -------------------
+
+print("\n")
+
+a=3
+b=5
+
+print(2*a*3*b)
+
+# Exercício 3 -------------------
+
+print("\n")
+
+x=3
+y=2
+z=7
+
+soma=x+y+z
+
+print(soma)

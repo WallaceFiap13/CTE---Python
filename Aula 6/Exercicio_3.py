aece1415dd715d287300741641e0ee6f8c41b75f
WallaceQ13 <wallace.lima6@etec.sp.gov.br>
2025-08-25 22:34:58 -0300

    25/08

--- Aula 6/Exercicio_3.py
+++ Aula 6/Exercicio_3.py
@@ -1,13 +1,15 @@
 Listaaa = [1,7,8,25,9,15]
 
-maximo = Listaaa[0]
-minimo = Listaaa[0]
+# maximo = Listaaa[0]
+# minimo = Listaaa[0]
 
-for e in Listaaa:
-    if e > maximo:
-        maximo = e     
-    if e < minimo:
-        minimo = e
-print(maximo)
-print(minimo)
+# for e in Listaaa:
+#     if e > maximo:
+#         maximo = e     
+#     if e < minimo:
+#         minimo = e
+# print(maximo)
+# print(minimo)
 
+print(max(Listaaa))
+print(min(Listaaa))
